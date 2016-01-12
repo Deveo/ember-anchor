0d3101814fe7f6fdf18b18c59e5e83026470fa2e
Egor Kalinichev <egork92@gmail.com>
2016-01-12 11:07:42 +0700

    Do not clear query params when entering the page

--- addon/mixins/controller-support.js
+++ addon/mixins/controller-support.js
@@ -12,13 +12,6 @@ export default Ember.Mixin.create({
   anchorQueryParam: oneWay('_anchorConfig.anchorQueryParam'),
   _anchorConfig: injectConfig(),
 
-  init() {
-    this._super(...arguments);
-    Ember.run.next(() => {
-      this.set(this.get('anchorQueryParam'), undefined);
-    });
-  },
-
   queryParams: computed('anchorQueryParam', function() {
     let qpValue = this.get('anchorQueryParam');
     return Ember.A(qpValue ? [qpValue] : []);
