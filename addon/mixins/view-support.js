633f67ef9a81a7e184ad7399e054ff04bace1d14
Ville Vainio <ville@deveo.com>
2017-04-04 15:12:22 +0300

    Make scrolling to elements more robust
    Fixes an issue with `scrollTop()` on `body` not functioning properly if the container height is restricted to e.g. `100vh`.
    Instead, use `scrollIntoView()` https://developer.mozilla.org/en/docs/Web/API/Element/scrollIntoView

--- addon/mixins/view-support.js
+++ addon/mixins/view-support.js
@@ -45,7 +45,7 @@ export default Mixin.create({
     let elem = $(`[data-${qp}="${qpVal}"]`);
     let offset = (elem && elem.offset && elem.offset()) ? elem.offset().top : null;
     if (offset) {
-      $('body').scrollTop(offset);
+      elem.get(0).scrollIntoView();
       this.trigger('scrolledToElement', elem);
     }
   }
