03631c134def4c06cd9bb26cefad80a8950dc513
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2017-10-05 16:07:55 +0300

    Do not offensively overwrite `this.queryParams` on controllers

--- addon/mixins/controller-support.js
+++ addon/mixins/controller-support.js
@@ -22,7 +22,15 @@ export default Mixin.create({
 
   init() {
     let qpValue = this.get('anchorQueryParam');
-    this.queryParams = qpValue ? [qpValue] : [];
+
+    if (qpValue) {
+      if (!this.queryParams) {
+        this.queryParams = [];
+      }
+
+      this.queryParams.addObject(qpValue);
+    }
+
     this._super(...arguments);
   }
 });
